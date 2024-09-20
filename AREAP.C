7bec3fc8c9338453101d34c21a33df148b6f02fe
Shruti200277 <tejas01022002@gmail.com>
2024-09-20 12:17:02 +0530

    Add files via upload

---
+++ AREAP.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=2,l=3,b=4,r=2,x=5,y=3,z=2;
+printf("PERIMETER OF SQUARE=%d",4*a);
+printf("AREA OF SQUARE=%d",a*a);
+printf("PERIMETER OF RECTANGLE=%d",2*(l+b));
+printf("AREA OF RECTANGLE=%d",l*b);
+printf("PERIMETER OF CIRCLE=%f",2*3.14*r);
+printf("AREA OF CIRCLE=%f",3.14*r*r);
+printf("PERIMETER OF TRIANGLE=%d",x+y+z);
+printf("AREA OF TRIANGLE=%d",(x*y)/2);
+getch();
+clrscr();
+}
---
+++ FLOAT.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a=3.34,b=2.22;
+printf("output =%.1f\n",a*b);
+printf("output=%.2f",a*b);
+getch();
+}
